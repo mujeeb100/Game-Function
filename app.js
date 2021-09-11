9b1363e2081e9cee77bec04085373e7d29a8b66c
muzeeb100 <muzeeb100@yahoo.co.in>
2021-09-11 09:36:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS ='COMPUTER_WINS';
+
+let gameIsRunning = false;
+
+
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK} , ${PAPER} Or ${SCISSORS}?`,'').toUpperCase();
+    if (
+        selection !== 'ROCK' &&
+        selection !== 'PAPER' &&
+        selection !== 'SCISSORS'
+    ) {
+        alert('Invalid choice! we chose ${DEFAULT_USER_CHOICE} ROCK for you!');
+    return  DEFAULT_USER_CHOICE;
+    }
+    return selection;
+    };
+
+    const getComputerChoice = function(){
+        const randomValue = Math.random();
+        if(randomValue < 0.34){
+            return ROCK;
+        } else if (randomValue < 0.67){
+            return PAPER;
+        }   else {
+            return SCISSORS;
+        }
+    };
+
+    const getWinner = function(cChoice,pChoice){
+        if (cChoice === pChoice){
+            return RESULT_DRAW;
+        }else if(cChoice === ROCK && pChoice===PAPER || 
+                 cChoice===SCISSORS && pChoice === SCISSORS ||
+                 cChoice === SCISSORS && pChoice === ROCK)
+                 {
+        return RESULT_PLAYER_WINS;
+                 }else {
+                    return RESULT_COMPUTER_WINS;
+                 }
+                };
+
+startGameBtn.addEventListener('click', function(){
+    if (gameIsRunning){
+    return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting..');
+    const playerChoice = getPlayerChoice();
+    //console.log(playerSelection);
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(winner);
+});
